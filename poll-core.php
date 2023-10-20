9cb06e8b4d62ade6783ff9b4d482049f3efe7ce9
0dinX <148571947+0dinX@users.noreply.github.com>
2023-10-20 19:27:45 +0300

    Add files via upload

---
+++ poll-core.php
@@ -0,0 +1,50 @@
+<div id="poll_title">Ce faci dacă vezi pe cineva mergând pe stradă cu sandale și șosete în picioare?</div><br />
+<div id="results" class="results"><div style="float: left;"><b>Sandale și șosete?? Obscen!!! </b></div>
+					<div style="float: right;"><font class="small">20.08%</font></div><br />
+					<div style="width: 100%;">
+					<div style="float: left;" class="barmax"  name="20.084898985929" id="poll_result">&nbsp;</div>
+					</div><br />
+					<div id='poll_separator'></div>
+<div style="float: left;"><b>Îi felicit gustul în materie de modă.</b></div>
+					<div style="float: right;"><font class="small">17.73%</font></div><br />
+					<div style="width: 100%;">
+					<div style="float: left;" class="bar"  name="17.730524329848" id="poll_result">&nbsp;</div>
+					</div><br />
+					<div id='poll_separator'></div>
+<div style="float: left;"><b>Mă duc să îmi cumpăr și eu.</b></div>
+					<div style="float: right;"><font class="small">17.26%</font></div><br />
+					<div style="width: 100%;">
+					<div style="float: left;" class="bar"  name="17.256242957839" id="poll_result">&nbsp;</div>
+					</div><br />
+					<div id='poll_separator'></div>
+<div style="float: left;"><b>Îl/o întreb daca îi LinusTechTips.</b></div>
+					<div style="float: right;"><font class="small">13.61%</font></div><br />
+					<div style="width: 100%;">
+					<div style="float: left;" class="bar"  name="13.607420800252" id="poll_result">&nbsp;</div>
+					</div><br />
+					<div id='poll_separator'></div>
+<div style="float: left;"><b>Mă mut pe trotuarul celălalt.</b></div>
+					<div style="float: right;"><font class="small">12.61%</font></div><br />
+					<div style="width: 100%;">
+					<div style="float: left;" class="bar"  name="12.609071613867" id="poll_result">&nbsp;</div>
+					</div><br />
+					<div id='poll_separator'></div>
+<div style="float: left;"><b>Aprind 2 lumânări la biserică.</b></div>
+					<div style="float: right;"><font class="small">10.23%</font></div><br />
+					<div style="width: 100%;">
+					<div style="float: left;" class="bar"  name="10.229803736604" id="poll_result">&nbsp;</div>
+					</div><br />
+					<div id='poll_separator'></div>
+<div style="float: left;"><b>Aprind o lumânare la biserică.</b></div>
+					<div style="float: right;"><font class="small">5.17%</font></div><br />
+					<div style="width: 100%;">
+					<div style="float: left;" class="bar"  name="5.173859497419" id="poll_result">&nbsp;</div>
+					</div><br />
+					<div id='poll_separator'></div>
+<div style="float: left;"><b>Le fac o poză să o pun pe facebook.</b></div>
+					<div style="float: right;"><font class="small">3.31%</font></div><br />
+					<div style="width: 100%;">
+					<div style="float: left;" class="bar"  name="3.3081780782433" id="poll_result">&nbsp;</div>
+					</div><br />
+					<div id='poll_separator'></div>
+</div><div style="padding: 15px;" align="center">Total Votes: <b>76326</b></div>
